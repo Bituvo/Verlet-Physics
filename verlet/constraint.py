93727b5479dc1bb7f01371ccc5f8f6fbca15d6c3
Thresher <90872694+Bituvo@users.noreply.github.com>
2022-11-11 14:59:59 -0500

    Add files via upload

---
+++ verlet/constraint.py
@@ -0,0 +1,36 @@
+from math import sqrt
+
+def distance(point1, point2):
+    return sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2)
+
+class Constraint:
+    def __init__(self, world, startPoint, endPoint, length=None, stiffness=0.3):
+        self.world = world
+
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+        self.length = distance(self.startPoint, self.endPoint) if not length else length
+        self.stiffness = stiffness
+
+    def update(self, repititions=1):
+        for i in range(repititions):
+            diffX = self.startPoint.x - self.endPoint.x
+            diffY = self.startPoint.y - self.endPoint.y
+            dist = distance(self.startPoint, self.endPoint)
+
+            difference = 0
+            if dist > 0:
+                difference = (self.length - dist) / dist
+
+            translateX = diffX * (difference / 2 * self.stiffness)
+            translateY = diffY * (difference / 2 * self.stiffness)
+
+            if not self.startPoint.pinned:
+                self.startPoint.x += translateX
+                self.startPoint.y += translateY
+                self.startPoint.applyBoundaries()
+
+            if not self.endPoint.pinned:
+                self.endPoint.x -= translateX
+                self.endPoint.y -= translateY
+                self.endPoint.applyBoundaries()
---
+++ verlet/node.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, world, x, y, velX=0, velY=0,
+                 pinned=False, radius=7):
+        self.world = world
+
+        self.x, self.y = x, y
+        self.velX, self.velY = velX, velY
+        self.oldX = self.x - self.velX
+        self.oldY = self.y - self.velY
+
+        self.pinned = pinned
+        self.radius = radius
+
+    def update(self):
+        if not self.pinned:
+            self.physicsStep()
+            self.applyBoundaries()
+
+    def physicsStep(self):
+        self.velX = (self.x - self.oldX) * self.world.airFriction
+        self.velY = (self.y - self.oldY) * self.world.airFriction
+
+        self.oldX, self.oldY = self.x, self.y
+        self.x += self.velX + self.world.gravityX
+        self.y += self.velY + self.world.gravityY
+
+    def applyBoundaries(self): self.world.boundaryFunction(self)
