3dac906c3a681389b5d0ead5e82ea8aa9b830e73
Thresher <90872694+Bituvo@users.noreply.github.com>
2022-11-20 08:16:32 -0500

    Delete world.py

--- verlet/world.py
+++
@@ -1,7 +0,0 @@
-class World:
-    def __init__(self, width, height, gravity, airFriction, boundaryFunction, timeStep):
-        self.width, self.height = width, height
-        self.gravityX, self.gravityY = gravity
-        self.airFriction = airFriction
-        self.boundaryFunction = boundaryFunction
-        self.timeStep = timeStep
