38af083e747dfdd5bdad01d565960e24d9d84e8d
Thresher <90872694+Bituvo@users.noreply.github.com>
2023-01-08 14:00:48 -0500

    Update node.py

--- verlet/node.py
+++ verlet/node.py
@@ -1,11 +1,10 @@
 class Node:
-    def __init__(self, world, x, y, xVel=0.0, yVel=0.0,
-                 pinned=False, radius=5.0):
+    def __init__(self, world, x, y, xVel, yVel, pinned, radius):
         self.world = world
 
         self.x, self.y = x, y
-        self.oldX = self.x - xVel
-        self.oldY = self.y + yVel
+        self.oldX = x - xVel * self.world.timeStep
+        self.oldY = y - yVel * self.world.timeStep
 
         self.pinned = pinned
         self.radius = radius
@@ -13,16 +12,13 @@ class Node:
     def update(self):
         if not self.pinned:
             self.physicsStep()
-            self.applyBoundaries()
+            self.world.boundaryFunction(self)
 
     def physicsStep(self):
         x, y = self.x, self.y
         oldX, oldY = self.oldX, self.oldY
         self.oldX, self.oldY = x, y
+
         airFriction, timeStep = self.world.airFriction, self.world.timeStep
-
-        self.x += (x - oldX) * airFriction - self.world.gravityX * timeStep
-        self.y += (y - oldY) * airFriction - self.world.gravityY * timeStep
-
-    def applyBoundaries(self):
-        self.world.boundaryFunction(self)
+        self.x += (x - oldX) * airFriction + self.world.gravityX * timeStep
+        self.y += (y - oldY) * airFriction + self.world.gravityY * timeStep
